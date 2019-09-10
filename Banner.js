0d4482330ac891755a6d7f3b82f8f83d66bffbbc
Giulio Fagioli <fagioli.giulio@gmail.com>
2019-09-10 12:04:44 +0200

    Add template structure

--- Banner.js
+++ Banner.js
@@ -1,13 +1,53 @@
-import {
-	BannerWrapper,
-	ImageWrapper,
-	AppName,
-	AppDescription,
-	InstallButton
-} from "./CustomComponents";
+import { h } from "preact";
+import { styled, setPragma } from "goober";
+setPragma(h);
+
+
+const BannerWrapper = styled("div")`
+  position: fixed;
+  padding: 1rem 1rem;
+  display: flex;
+  width: 100vw;
+  align-items: center;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  bottom: 0;
+  justify-content: space-between;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Helvetica Neue", Arial, sans-serif;
+`;
+
+const ImageWrapper = styled("figure")`
+  padding: 0.1rem;
+  width: 32px;
+  height: 32px;
+  margin: 0px;
+`;
+
+const AppName = styled("h2")`
+  font-size: 24px;
+`;
+
+const AppDescription = styled("p")`
+  font-size: 14px;
+`;
+
+const InstallButton = styled("button")`
+  border: none;
+  background: white;
+  color: black;
+`;
 
 function Banner({children, ...props}) {
-	return <BannerWrapper onClick={() => props.p(false)}>ciao</BannerWrapper>;
+	return (<BannerWrapper onClick={() => props.dismiss(false)}>
+			<ImageWrapper>
+				<img  className="banner__icon" />
+			</ImageWrapper>
+			<div>
+				<AppName>App Banner Preact</AppName>
+				<AppDescription>Piccola descrizione</AppDescription>
+			</div>
+			<InstallButton>Installa</InstallButton>
+		</BannerWrapper>);
 }
 
 export default Banner;
